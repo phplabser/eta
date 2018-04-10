6c0db702f4faa537670497d0e71691c371127a33
phplabser <ms@phplabs.pl>
2018-04-10 12:22:43 +0200

    Renderer - function load can return tpl content instead of outputing

--- library/View/Renderer.php
+++ library/View/Renderer.php
@@ -18,49 +18,54 @@ use Eta\Exception\RuntimeException;
 use Eta\Interfaces\PostRendererInterface;
 use Eta\Model\Singleton;
 
-class Renderer extends Singleton {
+class Renderer extends Singleton
+{
 
-    protected $params = [];
+    protected        $params = [];
     protected static $layout = null;
 
-    protected $pageTitle = "";
+    protected $pageTitle       = "";
     protected $pageDescription = "";
 
-    protected $helpers = [];
+    protected $helpers      = [];
     protected $postRenderer = [];
-    protected $useLayout = true;
+    protected $useLayout    = true;
 
     protected $forcedTemplate = null;
 
     protected $generatedTplVars = [];
 
-    public function addParams(Array $array) {
-        $this->params = array_merge($this->params,$array);
+    public function addParams(Array $array)
+    {
+        $this->params = array_merge($this->params, $array);
+
         return $this;
     }
 
-    public function setTemplate($template) {
+    public function setTemplate($template)
+    {
         $this->forcedTemplate = $template;
     }
 
-    public function render($route,$module,$params = [],$return=false) {
+    public function render($route, $module, $params = [], $return = false)
+    {
         $this->addParams($params);
         $layouts = Config::getInstance()->get('layouts');
 
         if (Response::getInstance()->getResponseType() == Response::STATUS_OK || Response::getInstance()->getResponseType() == Response::STATUS_REDIRECTION) {
-            if($this->forcedTemplate !== null) {
-                $templatePath = "application" . DIRECTORY_SEPARATOR . "module". DIRECTORY_SEPARATOR . $this->forcedTemplate . ".phtml";
+            if ($this->forcedTemplate !== null) {
+                $templatePath = "application" . DIRECTORY_SEPARATOR . "module" . DIRECTORY_SEPARATOR . $this->forcedTemplate . ".phtml";
             } else {
-                $templatePath = "application" . DIRECTORY_SEPARATOR . "module". DIRECTORY_SEPARATOR . $module. DIRECTORY_SEPARATOR . "views". DIRECTORY_SEPARATOR . strtolower($route['route']['controller']). DIRECTORY_SEPARATOR .strtolower($route['route']['action']).".phtml";
+                $templatePath = "application" . DIRECTORY_SEPARATOR . "module" . DIRECTORY_SEPARATOR . $module . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . strtolower($route['route']['controller']) . DIRECTORY_SEPARATOR . strtolower($route['route']['action']) . ".phtml";
             }
 
 
         } else {
-            if(isset($layouts['error'])) {
+            if (isset($layouts['error'])) {
                 $module = 'error';
             }
-            if (isset($layouts["error/".Response::getInstance()->getResponse()])) {
-                $templatePath = $layouts["error/".Response::getInstance()->getResponse()];
+            if (isset($layouts["error/" . Response::getInstance()->getResponse()])) {
+                $templatePath = $layouts["error/" . Response::getInstance()->getResponse()];
             } else {
                 if (isset($layouts["error"])) {
                     $templatePath = $layouts["error"];
@@ -81,95 +86,104 @@ class Renderer extends Singleton {
         $tpl = $this->includeTpl($templatePath);
         $this->initLayout($module);
 
-        if(!$this->useLayout) {
-            self::$layout = null;
+        if (!$this->useLayout) {
+            self::$layout    = null;
             $this->useLayout = true;
         }
 
-        if(self::$layout) {
+        if (self::$layout) {
             $this->params['templateContent'] = $tpl;
-            $tpl = $this->includeTpl(self::$layout . DIRECTORY_SEPARATOR . "layout.phtml");
+            $tpl                             = $this->includeTpl(self::$layout . DIRECTORY_SEPARATOR . "layout.phtml");
         }
 
-        if(count($this->postRenderer)) {
+        if (count($this->postRenderer)) {
             foreach ($this->postRenderer as $renderer) {
                 $renderer = new $renderer();
-                if(!($renderer instanceof PostRendererInterface)) {
+                if (!($renderer instanceof PostRendererInterface)) {
                     throw new \RuntimeException("PostRenderer must implements PostRendererInterface");
                 }
                 $tpl = $renderer->render($tpl);
             }
         }
-        if($return) return $tpl;
+        if ($return) return $tpl;
         echo $tpl;
     }
 
-    protected function includeTpl($tpl) {
+    protected function includeTpl($tpl)
+    {
         ob_start();
         $resp = include($tpl);
-        $tpl = ob_get_clean();
-        if(!$resp) {
-            Debug::raiseError("Missing template $tpl",Debug::ETA_ERROR_WARNING);
+        $tpl  = ob_get_clean();
+        if (!$resp) {
+            Debug::raiseError("Missing template $tpl", Debug::ETA_ERROR_WARNING);
+
             return "";
         }
+
         return $tpl;
     }
 
-    public function load($templateName,$layout = null) {
+    public function load($templateName, $layout = null, $returnAsString = false)
+    {
         $layout = $layout ?? self::$layout;
-        $tpl = $this->includeTpl($layout . DIRECTORY_SEPARATOR . $templateName . ".phtml");
+        $tpl    = $this->includeTpl($layout . DIRECTORY_SEPARATOR . $templateName . ".phtml");
 
+        if ($returnAsString) return $tpl;
         echo $tpl;
-        return;
-
-//        $result = @include($layout . DIRECTORY_SEPARATOR . $templateName . ".phtml");
-//        if(false === $result) {
-//            Debug::raiseError("Missing template $templateName (in $layout)",Debug::ETA_ERROR_WARNING);
-//        }
     }
 
-    public function registerHelper($name, $class) {
+    public function registerHelper($name, $class)
+    {
         $this->helpers[$name] = $class;
+
         return $this;
     }
 
-    public function registerPostRenderer($name, $class) {
+    public function registerPostRenderer($name, $class)
+    {
         $this->postRenderer[$name] = $class;
+
         return $this;
     }
 
-    public function __call($helper, $params) {
-        if(!isset($this->helpers[$helper])) {
+    public function __call($helper, $params)
+    {
+        if (!isset($this->helpers[$helper])) {
             throw new RuntimeException("View helper $helper not registered!");
         }
-        if(get_parent_class($this->helpers[$helper]) != "Eta\\View\\Helper") {
+        if (get_parent_class($this->helpers[$helper]) != "Eta\\View\\Helper") {
             throw new RuntimeException("Helper {$this->helpers[$helper]} must extends \\Eta\\View\\Helper!");
         }
         $helper = $this->helpers[$helper];
         $helper = $helper::getInstance();
+
         return $helper->execute(...$params);
     }
 
-    public function __get($name) {
-        if(isset($this->params[$name])) {
+    public function __get($name)
+    {
+        if (isset($this->params[$name])) {
             return $this->params[$name];
         }
     }
 
-    public function setLayout($layout) {
+    public function setLayout($layout)
+    {
         self::$layout = $layout;
     }
 
-    public function setNoLayout() {
+    public function setNoLayout()
+    {
         $this->useLayout = false;
     }
 
-    protected function initLayout($module) {
-        $module = strtolower($module);
+    protected function initLayout($module)
+    {
+        $module  = strtolower($module);
         $layouts = Config::getInstance()->get("layouts");
-        if(!$layouts) return $this;
+        if (!$layouts) return $this;
 
-        if(isset($layouts[$module])) {
+        if (isset($layouts[$module])) {
             self::$layout = $layouts[$module];
         } else {
             self::$layout = isset($layouts['default']) ? $layouts['default'] : null;
@@ -178,21 +192,27 @@ class Renderer extends Singleton {
         return $this;
     }
 
-    public function setPageTitle($title) {
+    public function setPageTitle($title)
+    {
         $this->pageTitle = $title;
+
         return $this;
     }
 
-    public function setPageDescription($description) {
+    public function setPageDescription($description)
+    {
         $this->pageDescription = $description;
+
         return $this;
     }
 
-    public function getPageTitle() {
+    public function getPageTitle()
+    {
         return $this->pageTitle;
     }
 
-    public function getPageDescription() {
+    public function getPageDescription()
+    {
         return $this->pageDescription;
     }
 
